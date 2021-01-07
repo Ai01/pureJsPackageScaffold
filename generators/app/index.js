546f53f8d53e463edca72db22b82d892d943e7d7
柏海辉 <baihaihui@baiHH.local>
2021-01-07 17:46:14 +0800

    chore: core refactor

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,7 +2,6 @@
 const Generator = require("yeoman-generator");
 const chalk = require("chalk");
 const yosay = require("yosay");
-const path = require("path");
 
 module.exports = class extends Generator {
   async prompting() {
